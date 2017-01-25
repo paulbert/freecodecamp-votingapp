e03030d5d107ce8dda6bcdea12e732a10cfd6f1b
paulbert <paul.r.albert@gmail.com>
2017-01-25 16:45:44 -0500

    Fix text change parameter order

--- src/components/EditPoll.js
+++ src/components/EditPoll.js
@@ -5,17 +5,17 @@ const EditPoll = ({poll,onTextChange,onPollSubmit,onAddOptClick}) => (
 	<form onSubmit={(e) => { e.preventDefault(); return onPollSubmit(poll.link, poll);}}>
 		<div className="form-group">
 			<label className="control-label">Poll Title:</label>
-			<input type="text" className="form-control" defaultValue={poll.title} onChange={(e) => onTextChange(e.target.value,'title')} />
+			<input type="text" className="form-control" defaultValue={poll.title} onChange={(e) => onTextChange('title',e.target.value)} />
 		</div>
 		<div className="form-group">
 			<label className="control-label">Poll description:</label>
-			<input type="text" className="form-control" defaultValue={poll.desc} onChange={(e) => onTextChange(e.target.value,'desc')} />
+			<input type="text" className="form-control" defaultValue={poll.desc} onChange={(e) => onTextChange('desc',e.target.value)} />
 		</div>
 		<div className="form-group">
 			<label className="control-label">Options:</label>
 			{
 				poll.options.map((val,ind) => (
-					<input type="text" className="form-control" defaultValue={val} onChange={(e) => onTextChange(e.target.value,'options',ind)} />
+					<input type="text" className="form-control" defaultValue={val} onChange={(e) => onTextChange('options',e.target.value,ind)} />
 				))
 			}
 		</div>
