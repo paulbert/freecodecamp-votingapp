9f5e621cab31fe7ee32370b9abd44e5d07181b0e
Paul Albert <paul.r.albert@gmail.com>
2017-02-20 14:00:37 -0500

    Remove sessions variable

--- routes/index.js
+++ routes/index.js
@@ -1,85 +1,27 @@
 
 var usersDAO = require('./usersDAO'),
 	path = require('path'),
-	pollsDAO = require('./pollsDAO'),
-	sessionsDAO = require('./sessionsDAO'),
-	crypto = require('crypto');
+	pollsDAO = require('./pollsDAO');
 	
 module.exports = exports = function(app,db) {
 	
 	var users = usersDAO(db),
-		polls = pollsDAO(db),
-		sessions = sessionsDAO(db),
-		cookieTime = 30 * 24 * 3600000;
-	
-	function addResponseCookie(res,user,passcode) {
-		res.cookie('user',user,{expires: new Date(Date.now() + cookieTime),httpOnly:false});
-		res.cookie('passcode',passcode,{expires: new Date(Date.now() + cookieTime),httpOnly:false});
-		res.set('Access-Control-Allow-Origin','*');
-		res.set('Access-Control-Allow-Credentials','true');
-	}
-	
-	function cookieCallback (err,newCookie,res) {
-		//console.log(newCookie);
-		if(err) {
-			res.json({'message':err});
-		} else {
-			addResponseCookie(res,newCookie.user_id,newCookie.passcode);
-			res.json({'message':'Success'});
-		}
-	}
-	
-	app.post('/signup', function(req,res) {
-		var user = req.body;
-		console.log(user);
-		users.insertUser(user,function(err,newUser) {
-			console.log('New User:');
-			console.log(newUser);
-			if(err) {
-				res.json({'message':err});
-			} else {
-				sessions.insertNewSession(newUser.ops[0],function(err,newCookie) {
-					console.log('Starting session...');
-					cookieCallback(err,newCookie.ops[0],res);
-				});
-			}
-		});
-	});
-	
-	app.post('/login', function(req,res) {
-		var user = req.body;
-		users.checkPassword(user,function(err,passed) {
-			if(passed) {
-				sessions.insertNewSession(user,function(err,newCookie) {
-					cookieCallback(err,newCookie.ops[0],res);
-				});
-			} else {
-				res.json({'message':'Username or password do not match'});
-			}
-		});	
-	});
+		polls = pollsDAO(db);
 	
 	app.post('/savePoll', function(req,res) {
 		console.log(req.body);
 		var poll = req.body.poll,
 			pollLink = req.body.pollLink || 'new',
-			user = req.cookies.user;
-		//console.log(user);
-		user = 'test';
-		sessions.checkCookie(req.cookies,function(foundCookie) {
-			if(foundCookie) {
-				polls.upsert(poll,pollLink,user,function(err,result) {
-					console.log('Attempting upsert...');
-					if(err) {
-						console.log(err);
-						res.json({'message':'Error'});
-					} else {
-						console.log('Poll upsert successful');
-						res.json({'message':'Success'});
-					}
-				});
+			user = req.user;
+		
+		polls.upsert(poll,pollLink,user,function(err,result) {
+			console.log('Attempting upsert...');
+			if(err) {
+				console.log(err);
+				res.json({'message':'Error'});
 			} else {
-				res.json({'message':'Unauthenticated'});
+				console.log('Poll upsert successful');
+				res.json({'message':'Success'});
 			}
 		});
 	});
