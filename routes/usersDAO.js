e8e452473b91d20d5305f95232213bff5a3e2c3b
Paul Albert <paul.r.albert@gmail.com>
2017-02-20 12:08:29 -0500

    Add get and insert user methods

--- routes/usersDAO.js
+++ routes/usersDAO.js
@@ -1,60 +1,19 @@
 
-function usersDAO (db,testUsers) {
+function usersDAO (db) {
 	
 	var collection = 'users';
 	
-	function insertNewTrash(url,userId,callback) {
-		db.collection(collection).insert({trash:url,userId:userId,date:new Date(Date.now())},callback);
+	function insertNewUser(newUser,callback) {
+		db.collection(collection).insert(newUser,callback);
 	}
 	
-	function getTrash(callback) {
-		
-		var trashArray = [],
-			userArray = [],
-			currentUser = '';
-		
-		function findUser (user,ind,users) {
-			return user._id === currentUser;
-		}
-		
-		function appendUserToTrash (post,ind,trash) {
-			currentUser = post.userId;
-			thisUser = userArray.filter(findUser);
-			//console.log(thisUser[0]);
-			//console.log(trash);
-			return Object.assign({},thisUser[0],post);
-		}
-		
-		function trashUserJoin (err,users) {
-			//console.log(users[0]);
-			var joinedTrash = [];
-			if(err) {
-				callback(err);
-			} else {
-				userArray = users;
-				joinedTrash = trashArray.map(appendUserToTrash);
-				//console.log(joinedTrash[0]);
-				callback(0,joinedTrash);
-			}
-		}
-		
-		function getUsers (err,trash) {
-			//console.log(trash[0]);
-			if(err) {
-				callback(err);
-			} else {
-				trashArray = trash;
-				db.collection(usersCollection).find({}).toArray(trashUserJoin);
-			}
-		}
-		
-		db.collection(collection).find({}).sort({'date': -1}).toArray(getUsers);
-		
+	function getUser(userId,callback) {
+		db.collection(collection).findOne({fb_id:userId},{_id:-1},callback);
 	}
 	
 	return {
-		insertNewTrash:insertNewTrash,
-		getTrash:getTrash
+		insert:insertNewUser,
+		getOne:getUser
 	}
 	
 }
